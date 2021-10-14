dfedcb887ee30e0ec3e0436194d08d70e56215cf
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2021-10-14 21:50:48 +0300

    Update problema8.py

--- problema8.py
+++ problema8.py
@@ -1,13 +1,22 @@
-a='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z '
-a1=a 
-a2=a
-a3=a
-for i in range(0,len(a1)-2,2):
-    a1[i]=chr(ord(a1[i])+1)
-print(a1)
-while (a2.find("Z")>=0):
-    a2[a2.find("Z")+1]="A"
-print(a2)
-while (a3.find(" ")>=0):
-    a3[a3.find(" ")+1]="-"
-print(a3)
+a=str(input("Introduceti sirul"))
+sir1=""
+sir2=""
+sir3=""
+for i in a:
+    if (ord(i)>=65)and(ord(i)<=89):
+        sir1+=chr(ord(i)+1)
+    else:
+        sir1+=i
+print("a)Inlocuieste litele de la A pana la Y cu urmatoarea litera: \n",sir1)
+for i in a:
+    if i=='Z':
+        sir2+="A"
+    else:
+        sir2+=i
+print("b)Inlocuieste litele de Z cu A: \n",sir2)
+for i in a:
+    if i==' ':
+        sir3+="-"
+    else:
+        sir3+=i
+print("c)Inlocuieste spatiu cu - : \n",sir3)
